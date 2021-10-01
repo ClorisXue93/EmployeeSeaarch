0812b4f5329cd78ec6b5f648d5dfdbba21ab80b3
Chen <89332989+ClorisXue93@users.noreply.github.com>
2021-10-01 01:47:57 -0700

    add h file

--- employee.h
+++ employee.h
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef struct {
     long number;
     char *name;
     char *phone;
     double salary;
-} Employee, *PtrToEmployee;
+}Employee, *PtrToEmployee;
 
 typedef const Employee *PtrToConstEmployee;
