2ce6a1247443b9b18d035d454ab5fb6c13b1a310
Chen <89332989+ClorisXue93@users.noreply.github.com>
2021-10-01 02:25:00 -0700

    for two

--- employeeMain.c
+++ employeeMain.c
@@ -5,6 +5,7 @@ int main (void) {
     PtrToEmployee searchEmployeeByName(const Employee table[], int sizeTable, char* nameToFind);
     PtrToEmployee searchEmployeeByPhone(const Employee table[], int sizeTable, char* phoneToFind);
     PtrToEmployee searchEmployeeBySalary(const Employee table[], int sizeTable, double salaryToFind);
+    
 
     extern Employee EmployeeTable[];
     extern const int EmployeeTableEntries;
